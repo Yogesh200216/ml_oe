205f4b2543d4c1e5a517a145a2a33cceea016971
Yogesh <81550052+Yogesh200216@users.noreply.github.com>
2023-01-09 10:09:03 +0530

    Update Addition.py

--- Addition.py
+++ Addition.py
@@ -12,11 +12,8 @@ b = st.number_input("Enter Second number")
 result = a+b;
 st.success("The result is {}".format(result))
 print(result)
-filename = "Addition_result.csv"
 
-with open(filename, 'w') as csvfile:
-  csvwriter = csv.writer(csvfile)
-  csvwriter.writerow(result)
+
 
 # In[ ]:
 
