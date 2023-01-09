67b4b579bede780f5545f2a8761f5eacaf2cd0b8
Yogesh <81550052+Yogesh200216@users.noreply.github.com>
2023-01-09 09:34:24 +0530

    Add files via upload

---
+++ yogesh.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as slt
+
+
+# In[2]:
+
+
+slt.title("Hamlet said…")
+slt.text("""
+To be, or not to be, that is the question:
+Whether 'tis nobler in the mind to suffer
+The slings and arrows of outrageous fortune,
+Or to take arms against a sea of troubles
+And by opposing end them.
+""")
+
+
+# In[ ]:
+
+
+
+
