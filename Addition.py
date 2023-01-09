5a22f442077fa4347141b7ee3e9489dae595f232
Yogesh <81550052+Yogesh200216@users.noreply.github.com>
2023-01-09 10:02:10 +0530

    Update Addition.py

--- Addition.py
+++ Addition.py
@@ -5,13 +5,18 @@
 
 
 import streamlit as st
+import csv
 a = st.number_input("Enter first number")
 b = st.number_input("Enter Second number")
 
 result = a+b;
 st.success("The result is {}".format(result))
 print(result)
+filename = "Addition_result.csv"
 
+with open(filename, 'w') as csvfile:
+  csvwriter = csv.writer(csvfile)
+  csvwriter.writeroe(result)
 
 # In[ ]:
 
